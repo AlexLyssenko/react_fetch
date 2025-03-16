eafbae15dcfd3c82f0f04c188d499ccd7e43c836
alyssenko <Alexandr.Lyssenko@dataart.com>
2025-03-16 19:02:29 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Helmet } from "react-helmet";
 import HomePage from "./HomePage.tsx";
 import DetailsPage from "./DetailsPage.tsx";
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path=":id" element={<DetailsPage/>}/>
-            </Routes>
-        </Router>
+        <>
+            <Helmet>
+                <title>Fetch react</title>
+                <meta name="google-site-verification" content="A_WMxsHStyuT_xuG0Wj6tzpFxeQqSAD0YyCCk27OgLI" />
+            </Helmet>
+
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path=":id" element={<DetailsPage/>}/>
+                </Routes>
+            </Router>
+        </>
     );
 };
 
