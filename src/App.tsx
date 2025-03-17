ebe7f3a2563a7b9af4bee5e4bbe7c579fb70164e
alyssenko <Alexandr.Lyssenko@dataart.com>
2025-03-17 14:48:42 +0500

    bing search

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const App = () => {
         <>
             <Helmet>
                 <meta name="google-site-verification" content="A_WMxsHStyuT_xuG0Wj6tzpFxeQqSAD0YyCCk27OgLI" />
+                <meta name="msvalidate.01" content="D131A4DC4BABA108E2607C4F1B370F25" />
                 <title>Fetch react</title>
             </Helmet>
 
